33f0bbe10cb068c27029af9253f83eb0767c9540
RamiroIsCoding <r.miguez.lauria@gmail.com>
2024-10-07 00:31:02 -0300

    added test to check brands page url

--- src/main/java/com/zebrunner/carina/demo/gui/Pages/Android/BrandPage.java
+++ src/main/java/com/zebrunner/carina/demo/gui/Pages/Android/BrandPage.java
@@ -1,8 +1,11 @@
 package com.zebrunner.carina.demo.gui.Pages.Android;
 
 import com.zebrunner.carina.demo.gui.Pages.common.BrandPageBase;
+import com.zebrunner.carina.demo.gui.Pages.common.HomePageBase;
+import com.zebrunner.carina.utils.factory.DeviceType;
 import org.openqa.selenium.WebDriver;
 
+@DeviceType(pageType = DeviceType.Type.ANDROID_PHONE, parentClass = BrandPageBase.class)
 public class BrandPage extends BrandPageBase {
     public BrandPage(WebDriver driver) {
         super(driver);
--- src/main/java/com/zebrunner/carina/demo/gui/Pages/Desktop/BrandPage.java
+++ src/main/java/com/zebrunner/carina/demo/gui/Pages/Desktop/BrandPage.java
@@ -1,8 +1,11 @@
 package com.zebrunner.carina.demo.gui.Pages.Desktop;
 
 import com.zebrunner.carina.demo.gui.Pages.common.BrandPageBase;
+import com.zebrunner.carina.demo.gui.Pages.common.HomePageBase;
+import com.zebrunner.carina.utils.factory.DeviceType;
 import org.openqa.selenium.WebDriver;
 
+@DeviceType(pageType = DeviceType.Type.DESKTOP, parentClass = BrandPageBase.class)
 public class BrandPage extends BrandPageBase {
     public BrandPage(WebDriver driver) {
         super(driver);
